9acae425914b48d2ff14c8ca78cfe2ed02f77685
AMAN PRAJAPAT <98727830+aman-prajapat@users.noreply.github.com>
2023-01-06 17:14:55 +0530

    first commit

---
+++ Square_using_turtle.py
@@ -0,0 +1,17 @@
+import turtle
+pointer = turtle.Turtle()
+
+# for i in range(4):
+#         pointer.fd(100)
+#         pointer.lt(90)
+# # fd,bk,lt,rt
+
+def square(tur):
+    for i in range(4):
+        pointer.fd(100)
+        pointer.lt(90)
+
+square(pointer)
+
+
+    
